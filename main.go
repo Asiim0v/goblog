ff2d2e078757e71d44cd4685160efd1dd41fd07f
Asiim0v <664251936@qq.com>
2022-01-26 11:48:41 +0800

    add 404 http status code

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func handlerfunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
 			"<a href=\"mailto:Asiim0v@example.com\">Asiim0v@example.com</a>")
 	} else {
+		w.WriteHeader(http.StatusNotFound) // 添加 404 状态码
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
 	}
