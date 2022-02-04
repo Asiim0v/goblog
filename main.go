25a1b8908fc80c86c7e616948c1a12717e2084be
Asiim0v <664251936@qq.com>
2022-02-05 00:46:27 +0800

    refactor RouteName2URL

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"goblog/pkg/route"
 	"html/template"
 	"log"
 	"net/http"
@@ -17,7 +18,7 @@ import (
 )
 
 // gorilla/mux 因实现了 net/http 包的 http.Handler 接口，故兼容 http.ServeMux
-var router = mux.NewRouter()
+var router *mux.Router
 
 // 建立数据库连接
 var db *sql.DB
@@ -51,17 +52,6 @@ func (a Article) Link() string {
 		return ""
 	}
 	return showURL.String()
-}
-
-// RouteName2URL 通过路由名称来获取 URL
-func RouteName2URL(routeName string, pairs ...string) string {
-	url, err := router.Get(routeName).URL(pairs...)
-	if err != nil {
-		checkError(err)
-		return ""
-	}
-
-	return url.String()
 }
 
 // Int64ToString 将 int64 转换为 string
@@ -141,7 +131,7 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 4. 读取成功， 显示文章
 		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
-			"RouteName2URL": RouteName2URL,
+			"RouteName2URL": route.Name2URL,
 			"Int64ToString": Int64ToString,
 		}).ParseFiles("resources/views/articles/show.gohtml")
 		checkError(err)
@@ -498,6 +488,9 @@ func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	initDB()
 	createTables()
+
+	route.Initialize()
+	router = route.Router
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
