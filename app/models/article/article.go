5af976ad3101527c3a4152c7e1778436143a4ad4
Asiim0v <664251936@qq.com>
2022-02-05 15:23:15 +0800

    refactor article index page

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -1,8 +1,22 @@
 package article
 
+import (
+	"goblog/pkg/route"
+	"strconv"
+)
+
 // Article 文章类型
 type Article struct {
 	ID    uint64
 	Title string
 	Body  string
+}
+
+// Link 方法用来生成文章链接
+func (article Article) Link() string {
+	return route.Name2URL("articles.show", "id", article.GetStringID())
+}
+
+func (article Article) GetStringID() string {
+	return strconv.FormatUint(article.ID, 10)
 }
