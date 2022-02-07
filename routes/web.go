8c9128116141565a219661f87a8250c7bf2f770e
Asiim0v <664251936@qq.com>
2022-02-08 00:50:29 +0800

    add Login status in RenderTemplate

--- routes/web.go
+++ routes/web.go
@@ -14,11 +14,11 @@ func RegisterWebRoutes(r *mux.Router) {
 	// 静态页面相关，实例化 controllers.PagesController 后，将对应的方法指定到对应路由上即可
 	pc := new(controllers.PagesController)
 	r.NotFoundHandler = http.HandlerFunc(pc.NotFound)
-	r.HandleFunc("/", pc.Home).Methods("GET").Name("home")
 	r.HandleFunc("/about", pc.About).Methods("GET").Name("about")
 
 	// 文章相关页面
 	ac := new(controllers.ArticlesController)
+	r.HandleFunc("/", ac.Index).Methods("GET").Name("home")
 	r.HandleFunc("/articles/{id:[0-9]+}", ac.Show).Methods("GET").Name("articles.show")
 	r.HandleFunc("/articles", ac.Index).Methods("GET").Name("articles.index")
 
