783bf275ddd371a0c1d966190a677a55d3aa8089
Asiim0v <664251936@qq.com>
2022-02-07 22:58:37 +0800

    Auth pkg and DoLogin

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"goblog/app/models/user"
 	"goblog/app/requests"
+	"goblog/pkg/auth"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -14,7 +15,10 @@ type AuthController struct {
 
 // Register 注册页面
 func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
-	view.RenderSimple(w, view.D{}, "auth.register")
+	view.RenderSimple(w, view.D{
+		"Errors": make(map[string][]string),
+		"User":   user.User{},
+	}, "auth.register")
 }
 
 // DoRegister 处理注册逻辑
@@ -52,10 +56,30 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 
 // Login 显示登录表单
 func (*AuthController) Login(w http.ResponseWriter, r *http.Request) {
-	view.RenderSimple(w, view.D{}, "auth.login")
+	view.RenderSimple(w, view.D{
+		"Error":    nil,
+		"Email":    "",
+		"Password": "",
+	}, "auth.login")
 }
 
 // DoLogin 处理登录表单提交
 func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
-	//
+
+	// 1. 初始化表单数据
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+
+	// 2. 尝试登录
+	if err := auth.Attempt(email, password); err == nil {
+		// 登录成功
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		// 3. 失败, 显示错误提示
+		view.RenderSimple(w, view.D{
+			"Error":    err.Error(),
+			"Email":    email,
+			"Password": password,
+		}, "auth.login")
+	}
 }
