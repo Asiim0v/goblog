119444261bcb7faf771d17509169494c6b65352a
Asiim0v <664251936@qq.com>
2022-02-28 22:55:17 +0800

    edit

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -24,7 +24,7 @@ func initDB() {
 	// 设置数据库连接信息
 	config := mysql.Config{
 		User:                 "root",
-		Passwd:               "535128",
+		Passwd:               "",
 		Addr:                 "127.0.0.1:3306",
 		Net:                  "tcp",
 		DBName:               "goblog",
